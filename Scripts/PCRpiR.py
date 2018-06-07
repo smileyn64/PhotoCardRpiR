f3a1c8995e2a0783a22b1412a5c7341783a3493b
smileyn64 <loustaunau.nicolas@gmail.com>
2018-06-07 22:43:54 +0200

    Second Commit
    Début d’écriture du script principal.

---
+++ Scripts/PCRpiR.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import sys
+
+import dot3k.backlight as backlight
+import dot3k.joystick as nav
+import dot3k.lcd as lcd
+from dot3k.menu import Menu, MenuOption
+
+# Add the root examples dir so Python can find the plugins
+sys.path.append('../../')
+
+from plugins.utils import Backlight, Contrast
+
+
+#Classes Declaration
+# TODO: Faire les classes de menus
+class Idle(MenuOption):
+    def __init__(self, arg):
+        super(Idle, self).__init__()
+        self.arg = arg
+
+class State(MenuOption):
+    def __init__(self,arg):
+        super(State, self).__init__()
+        self.arg = arg
+
+class Copy(MenuOption):
+    def __init__(self, arg):
+        super(Copy, self).__init__()
+        self.arg = arg
+
+MyIdle = Idle()
+MyState = State()
+MyCopy = Copy()
+
+# Menu argument: structure, lcd, idle_handler = None, idle_time = 60
+menu = Menu({
+        'Etat': MyState,
+        'Copie': MyCopy,
+        'Options': {
+            'Display': {
+                'Contrast': Contrast(lcd),
+                'Backlight': Backlight(backlight)
+                        },
+                    },
+            },lcd,MyIdle,30)
+
+# Menu Command
+
+repeat_delay = 0.5
+
+@nav.on(nav.UP)
+def handle_up(pin):
+    menu.up()
+    nav.repeat(nav.UP, menu.up, repeat_delay, 0.9)
+
+@nav.on(nav.DOWN)
+def handle_down(pin):
+    menu.down()
+    nav.repeat(nav.DOWN, menu.down, repeat_delay, 0.9)
+
+@nav.on(nav.LEFT)
+def handle_left(pin):
+    menu.left()
+    nav.repeat(nav.LEFT, menu.left, repeat_delay, 0.9)
+
+@nav.on(nav.RIGHT)
+def handle_right(pin):
+    menu.right()
+    nav.repeat(nav.RIGHT, menu.right, repeat_delay, 0.9)
+
+@nav.on(nav.BUTTON)
+def handle_button(pin):
+    menu.select()
+
+# Loop
+while 1:
+    menu.redraw()
+    time.sleep(0.05)
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # PhotoCardRpiR
 
-# Avant Propos
+## Avant Propos
 L'objectif de ce projet et de créer un videur de carte autonome et nomade basé sur un Raspberry pi.
 Le but est de garder le videur de carte le plus simple possible pour une sauvegarde incrémentale des fichiers.
 
@@ -13,3 +13,6 @@ Le but est de garder le videur de carte le plus simple possible pour une sauvega
 ## Logiciel
 * Raspbian Lite
 * Python 3
+
+## Installation
+...
